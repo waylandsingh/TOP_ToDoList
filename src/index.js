94f8517b8465705970a193c65798cfaab896f2cd
waylandsingh <wmsingh@ucdavis.edu>
2022-08-03 13:37:06 -0700

    pre pubsub pattern tests

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import task from './task';
 import renderTask from './renderTask';
 import project from './project';
 import renderProject from './renderProject';
+import Pubsub from './Pubsub'
 
 
 const container = document.createElement("div");
@@ -23,18 +24,23 @@ const exampletasktoo = {
 
 const t1 = task(exampletask)
 
-// render test
-container.appendChild(renderTask(t1.getAllDetails()));
-console.log(t1.getID())
-// modifying a given task
-let testTasklist = [t1]
-let meh = project();
-meh.addTask(t1)
-// meh.selectTask(blah.getID()).setName('okay bruv') // successful demo of getting the name and setting it from outside of projects
-// console.log(meh.selectTask(blah.getID()).getName())
+//pubsub pattern test here
 
-// project rendering 
-// where do event listeners for updating fall into the mix?
+
+// render test
+// container.appendChild(renderTask(t1.getAllDetails()));
+// console.log(t1.getID())
+// modifying a given task
+// let testTasklist = [t1]
+// let meh = project();
+// meh.addTask(t1)
+// // meh.selectTask(blah.getID()).setName('okay bruv') // successful demo of getting the name and setting it from outside of projects
+// // console.log(meh.selectTask(blah.getID()).getName())
+
+// // project rendering 
+// // where do event listeners for updating fall into the mix?
+
+
 // meh.addTask(exampletasktoo)
 // const demoTaskList = meh.sortTaskList().map((t)=>t.getAllDetails())
 // console.log(...demoTaskList)
