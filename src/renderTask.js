6800ff42db83bc1286f3980669db9c6096536102
waylandsingh <wmsingh@ucdavis.edu>
2022-08-10 15:40:45 -0700

    Eventlistenersbuttons pubsubd to task props div rerendered

--- src/renderTask.js
+++ src/renderTask.js
@@ -9,51 +9,92 @@ export default function renderTask(container,
     setTaskPriority
     ) {
     // probably wise to separate this into own module
-    const renderTaskHTML = ({ID, name, desc, due, priority}) => {
-        
-        taskDiv.innerHTML = `
+    const renderTaskHTML = ({ID, name, desc, due, priority}) => {   
+        taskDetails.innerHTML = `
             <span class='task-name'>${name}</span>
             <span class='task-priority'>${priority}</span>
             <span class='task-due'>${due}</span>
             <p>${desc}</p>
         `
+            
     return 
     } 
 
     const nameChange = () => {
         console.log(ID, name, desc, due, priority)
         const newName = prompt("new name plz")
-        console.log(newName)
         pubsub.publish(ID+"_name", newName)
         console.log(getTaskDetails(task)) //is in same lexical scope as the outer task
         renderTaskHTML(getTaskDetails(task))
     }
 
+    const descChange = () => {
+        console.log(ID, name, desc, due, priority)
+        const newDesc = prompt("new name plz")
+        pubsub.publish(ID+"_desc", newDesc)
+        console.log(getTaskDetails(task)) //is in same lexical scope as the outer task
+        renderTaskHTML(getTaskDetails(task))
+    }
 
-    
+    const dueChange = () => {
+        console.log(ID, name, desc, due, priority)
+        const newDue = prompt("new due plz")
+        pubsub.publish(ID+"_due", newDue)
+        console.log(getTaskDetails(task)) //is in same lexical scope as the outer task
+        renderTaskHTML(getTaskDetails(task))
+    }
+
+    const priorityChange = () => {
+        console.log(ID, name, desc, due, priority)
+        const newPrio = prompt("new priority plz")
+        pubsub.publish(ID+"_priority", newPrio)
+        console.log(getTaskDetails(task)) //is in same lexical scope as the outer task
+        renderTaskHTML(getTaskDetails(task))
+    }
+
     // Future - deal with this dependency on task/getTaskDetails
-    var {ID, name, desc, due, priority} = getTaskDetails(task)
+    let {ID, name, desc, due, priority} = getTaskDetails(task)
 
     //create the task container and populate it with the task info
     let taskDiv = document.createElement("div")
     taskDiv.classList = 'task'
     taskDiv.id = ID
+    const taskDetails = document.createElement("div")
+    taskDetails.id = ID + 'details'
+    taskDiv.appendChild(taskDetails)
+    
+    //initial render of the task object
     renderTaskHTML(getTaskDetails(task))
 
     // pubsub manages updates in task properties
     const pubsub = new Pubsub()
     
     pubsub.subscription(ID + "_name", setTaskName).subscribe()
+    pubsub.subscription(ID + "_desc", setTaskDesc).subscribe()
+    pubsub.subscription(ID + "_due", setTaskDue).subscribe()
+    pubsub.subscription(ID + "_priority", setTaskPriority).subscribe()
+
     
-    // add event listener to take input, call publish changes, rerender
+    // add event listeners to take input, call publish changes, rerender
     const editNameButton = document.createElement("button")
     editNameButton.innerText = 'edit taskName'
-
     taskDiv.appendChild(editNameButton)
     editNameButton.addEventListener("click", nameChange)
 
-    
-    console.log(getTaskDetails(task))
-    
+    const editDescButton = document.createElement("button")
+    editDescButton.innerText = 'edit Description'
+    taskDiv.appendChild(editDescButton)
+    editDescButton.addEventListener("click", descChange)
+
+    const editDueButton = document.createElement("button")
+    editDueButton.innerText = 'edit Due'
+    taskDiv.appendChild(editDueButton)
+    editDueButton.addEventListener("click", dueChange)
+
+    const editPrioButton = document.createElement("button")
+    editPrioButton.innerText = 'edit Priority'
+    taskDiv.appendChild(editPrioButton)
+    editPrioButton.addEventListener("click", priorityChange)
+
     container.appendChild(taskDiv)
 }
