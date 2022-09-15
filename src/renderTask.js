8df5fa80f94c513cf696d86eced1a7cc5450a35b
Wayland Singh <waylandsingh@Waylands-MacBook-Air.local>
2022-09-15 15:12:21 -0700

    removed useless button/eventlisteners

--- src/renderTask.js
+++ src/renderTask.js
@@ -86,38 +86,8 @@ export default function renderTask(container, task) {
     // pubsub.publish(ID + "_name", task, newName);
     // console.log(getTaskDetails(task)); //is in same lexical scope as the outer task
   };
+
   
-  // const nameChange = () => {
-  //   console.log(ID, name, desc, due, priority);
-  //   const newName = prompt("new name plz");
-  //   pubsub.publish(ID + "_name", task, newName);
-  //   console.log(getTaskDetails(task)); //is in same lexical scope as the outer task
-  //   renderTaskHTML(getTaskDetails(task));
-  // };
-  // const descChange = () => {
-  //   console.log(ID, name, desc, due, priority);
-  //   const newDesc = prompt("new name plz");
-  //   pubsub.publish(ID + "_desc", task, newDesc);
-  //   console.log(getTaskDetails(task)); //is in same lexical scope as the outer task
-  //   renderTaskHTML(getTaskDetails(task));
-  // };
-
-  // const dueChange = () => {
-  //   console.log(ID, name, desc, due, priority);
-  //   const newDue = prompt("new due plz");
-  //   pubsub.publish(ID + "_due", task, newDue);
-  //   console.log(getTaskDetails(task)); //is in same lexical scope as the outer task
-  //   renderTaskHTML(getTaskDetails(task));
-  // };
-
-  // const priorityChange = () => {
-  //   console.log(ID, name, desc, due, priority);
-  //   const newPrio = prompt("new priority plz");
-  //   pubsub.publish(ID + "_priority", task, newPrio);
-  //   console.log(getTaskDetails(task)); //is in same lexical scope as the outer task
-  //   renderTaskHTML(getTaskDetails(task));
-  // };
-
   // Future - deal with this dependency on task/getTaskDetails
   let { ID, name, desc, due, priority } = getTaskDetails(task);
   console.log(getTaskDetails(task));
@@ -144,27 +114,6 @@ export default function renderTask(container, task) {
   editAllButton.innerText = 'Change em All!'
   taskDiv.appendChild(editAllButton)
   editAllButton.addEventListener('click', allChange)
-
-  // // add event listeners to take input, call publish changes, rerender
-  // const editNameButton = document.createElement("button");
-  // editNameButton.innerText = "edit taskName";
-  // taskDiv.appendChild(editNameButton);
-  // editNameButton.addEventListener("click", nameChange);
-
-  // const editDescButton = document.createElement("button");
-  // editDescButton.innerText = "edit Description";
-  // taskDiv.appendChild(editDescButton);
-  // editDescButton.addEventListener("click", descChange);
-
-  // const editDueButton = document.createElement("button");
-  // editDueButton.innerText = "edit Due";
-  // taskDiv.appendChild(editDueButton);
-  // editDueButton.addEventListener("click", dueChange);
-
-  // const editPrioButton = document.createElement("button");
-  // editPrioButton.innerText = "edit Priority";
-  // taskDiv.appendChild(editPrioButton);
-  // editPrioButton.addEventListener("click", priorityChange);
 
   container.appendChild(taskDiv);
 }
